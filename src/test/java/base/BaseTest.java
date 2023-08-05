01b13112542be1b502aed2ad5f0555649dde9774
Aleksandra Karic <aleksandrav86@gmail.com>
2023-08-05 23:20:32 +0200

    add WebDriver, WebDriverWait, Actions, and annotations @Before and @After

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,36 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+    public static WebDriverWait wdWait;
+    public static Actions actions;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver( co );
+        wdWait = new WebDriverWait(driver, Duration.ofSeconds(25));
+        actions = new Actions(driver);
+
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+    @After
+    public void tearDown(){ //TODO podsetnik: Otkomentarisi ovo ispod kad zavrsis pisanje svih testova
+        //driver.close(); // zatvara tab koji je trenutno u fokusu
+        //driver.quit();  // zatvara ceo browser
+    }
+}
